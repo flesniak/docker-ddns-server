ef9f906e9bf5585897f3e94ebe2893b7f1244701
Fabian Lesniak <fabian@lesniak-it.de>
2024-05-19 01:23:49 +0200

    use alpine as base image

--- deployment/s6-overlay/s6-rc.d/setup/setup.sh
+++ deployment/s6-overlay/s6-rc.d/setup/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 [ -z "$DDNS_DOMAINS" ] && echo "DDNS_DOMAINS not set" && exit 1
 [ -z "$DDNS_PARENT_NS" ] && echo "DDNS_PARENT_NS not set" && exit 1
@@ -9,24 +9,25 @@
 
 for d in ${DDNS_DOMAINS//,/ }
 do
-        if ! grep 'zone "'$d'"' /etc/bind/named.conf > /dev/null
+        if ! grep -sq 'zone "'$d'"' /etc/bind/named.conf.ddns
         then
                 echo "creating zone...";
-                cat >> /etc/bind/named.conf <<EOF
+                cat >> /etc/bind/named.conf.ddns <<EOF
 zone "$d" {
         type master;
-        file "$d.zone";
+        file "pri/$d.zone";
         allow-query { any; };
         allow-transfer { ${DDNS_TRANSFER}; };
         allow-update { localhost; };
+        also-notify { ${DDNS_NOTIFY} };
 };
 EOF
         fi
 
-        if [ ! -f /var/cache/bind/$d.zone ]
+        if [ ! -f /var/bind/pri/$d.zone ]
         then
                 echo "creating zone file..."
-                cat > /var/cache/bind/$d.zone <<EOF
+                cat > /var/bind/pri/$d.zone <<EOF
 \$ORIGIN .
 \$TTL 86400     ; 1 day
 $d              IN SOA  ${DDNS_PARENT_NS}. root.${d}. (
@@ -44,8 +45,5 @@ EOF
         fi
 done
 
-# If /var/cache/bind is a volume, permissions are probably not ok
-chown root:bind /var/cache/bind
-chown bind:bind /var/cache/bind/*
-chmod 770 /var/cache/bind
-chmod 644 /var/cache/bind/*
+# hand over zone files to the bind user and allow creating jnl files
+chown -R named:named /var/bind/pri
--- dyndns/go.mod
+++ dyndns/go.mod
@@ -21,7 +21,7 @@ require (
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
-	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.19 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
